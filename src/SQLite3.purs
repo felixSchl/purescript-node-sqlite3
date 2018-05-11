d9d6f08282da23d31f662f7387359bf218eb02f4
justinwoo <moomoowoo@gmail.com>
2018-05-11 15:10:36 +0300

    updates for 0.12

--- src/SQLite3.purs
+++ src/SQLite3.purs
@@ -2,43 +2,41 @@ module SQLite3 where
 
 import Prelude
 
-import Control.Monad.Aff (Aff, makeAff)
-import Control.Monad.Eff (Eff, kind Effect)
-import Data.Foreign (Foreign)
-import Data.Function.Uncurried (runFn4, runFn2, Fn4, Fn2)
-import Data.Monoid (mempty)
+import Data.Function.Uncurried as FU
+import Effect (Effect)
+import Effect.Aff (Aff, makeAff)
+import Foreign (Foreign)
 
 type FilePath = String
 type Query = String
 type Param = String
 
 foreign import data DBConnection :: Type
-foreign import data DBEffects :: Effect
 
-foreign import _newDB :: forall e.
-  Fn2
+foreign import _newDB ::
+  FU.Fn2
     FilePath
-    (DBConnection -> Eff (db :: DBEffects | e) Unit)
-  (Eff (db :: DBEffects | e) Unit)
-foreign import _closeDB :: forall e.
-  Fn2
+    (DBConnection -> Effect Unit)
+    (Effect Unit)
+foreign import _closeDB ::
+  FU.Fn2
     DBConnection
-    (Unit -> Eff (db :: DBEffects | e) Unit)
-    (Eff (db :: DBEffects | e) Unit)
-foreign import _queryDB :: forall e.
-  Fn4
+    (Unit -> Effect Unit)
+    (Effect Unit)
+foreign import _queryDB ::
+  FU.Fn4
     DBConnection
     Query
     (Array Param)
-    (Foreign -> Eff (db :: DBEffects | e) Unit)
-  (Eff (db :: DBEffects | e) Unit)
+    (Foreign -> Effect Unit)
+    (Effect Unit)
 
-newDB :: forall e. FilePath -> Aff (db :: DBEffects | e) DBConnection
+newDB :: FilePath -> Aff DBConnection
 newDB path =
-  makeAff \cb -> mempty <$ runFn2 _newDB path (cb <<< pure)
-closeDB :: forall e. DBConnection -> Aff (db :: DBEffects | e) Unit
+  makeAff \cb -> mempty <$ FU.runFn2 _newDB path (cb <<< pure)
+closeDB :: DBConnection -> Aff Unit
 closeDB conn =
-  makeAff \cb -> mempty <$ runFn2 _closeDB conn (cb <<< pure)
-queryDB :: forall e. DBConnection -> Query -> Array Param -> Aff (db :: DBEffects | e) Foreign
+  makeAff \cb -> mempty <$ FU.runFn2 _closeDB conn (cb <<< pure)
+queryDB :: DBConnection -> Query -> Array Param -> Aff Foreign
 queryDB conn query params =
-  makeAff \cb -> mempty <$ runFn4 _queryDB conn query params (cb <<< pure)
+  makeAff \cb -> mempty <$ FU.runFn4 _queryDB conn query params (cb <<< pure)
